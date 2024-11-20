951c0b9ea8513d5b26291d39f2b045d99a13379e
Harjot Singh Raith <144097963+Harjotraith04@users.noreply.github.com>
2024-11-20 08:08:21 +0530

    first commit

---
+++ frontend/src/Pages/InterviewRoom.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const InterviewRoom = () => {
+    return (
+      <div style={{ position: 'relative', marginTop: '46px' }}>
+      <iframe 
+          src="https://code-sync-live.vercel.app/" 
+          style={{ width: '100%', height: '95vh', border: 'none' }} 
+          title="Interview Room"
+      />
+     
+  </div>
+      )
+}
+
+export default InterviewRoom
---
+++ frontend/src/components/Common/DisclaimerBanner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DisclaimerBanner() {
+  return (
+    <div className="bg-red-300 text-black py-px px-4 w-full text-center mb-5 font-xs sm:font-sm md:font-base lg:font-lg xl:font-xl">
+      
+    </div>
+  );
+}
+
+export default DisclaimerBanner;
